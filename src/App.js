87208e705f2bf661d465d12cbdd0bb50fee6312e
r norman <rochellenorman0@gmail.com>
2020-01-06 16:39:01 +0000

    create layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import Gooofellas from './assets/Goodfellas.jpg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-Container">
+    <div className="First-Row">
+    <div className="First-Movie">
+    <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+    </div>
+     <div className="Second-Movie">
+     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     </div>
+     <div className="Third-Movie">
+     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     </div>
      <h1>Welcome to my app</h1>
-     <img className = "Cover-Image" src={Gooofellas} alt=''></img>
+     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     </div>
+     <div className="Second-Row">
+     <div className="First-Movie">
+    <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+    </div>
+     <div className="Second-Movie">
+     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     </div>
+     <div className="Third-Movie">
+     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     </div>
+     </div>
+     <div className="Third-Row">
+     <div className="First-Movie">
+     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     </div>
+      <div className="Second-Movie">
+      <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+      </div>
+      <div className="Third-Movie">
+      <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+      </div>
+     </div>
     </div>
   );
 }
