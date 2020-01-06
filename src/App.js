714afadbc7a0990c7549b4310be04b5e911d589b
r norman <rochellenorman0@gmail.com>
2020-01-06 16:18:16 +0000

    add first movie image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
+import Gooofellas from './assets/Goodfellas.jpg'
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to my app</h1>
+     <img className = "Cover-Image" src={Gooofellas} alt=''></img>
     </div>
   );
 }
 
 export default App;
+
+
