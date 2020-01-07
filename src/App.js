1dd43c92812ede8be4ca5db519e39cc9a72c559e
r norman <rochellenorman0@gmail.com>
2020-01-07 21:14:14 +0000

    add overlay

--- src/App.js
+++ src/App.js
@@ -11,43 +11,42 @@ import './App.css'
 
 function App() {
   return (
-    <div className="Container">
+<div className="Container">
     <div className="RowContainer">
-    <div className="PosterContainer">
-    <img className = "CoverImage" src={Gooofellas} alt='Goofellas Poster'></img>
-    <div className ="Title"><h1>Goodfellas</h1></div>
-    </div>
-    <div className="PosterContainer">
-    <img className = "CoverImage" src={Donnie} alt='Donnie Darko Poster'></img>
-    <div className ="Title"><h1>Donnie Darko</h1></div>
-    </div>
-    <div className="PosterContainer">
-     <img className = "CoverImage" src={Leon} alt='Leon Poster'></img>
-     <div className ="Title"><h1>Leon</h1></div>
+      <div className="PosterContainer">
+        <img className = "CoverImage" src={Gooofellas} alt='Goofellas Poster'></img>
+          <div className ="Title"><h1>Goodfellas</h1></div>
+      </div>
+        <div className="PosterContainer">
+          <img className = "CoverImage" src={Donnie} alt='Donnie Darko Poster'></img>
+            <div className ="Title"><h1>Donnie Darko</h1></div>
+        </div>
+        <div className="PosterContainer">
+          <img className = "CoverImage" src={Leon} alt='Leon Poster'></img>
+            <div className ="Title"><h1>Leon</h1></div>
+      </div>
+        <div className="PosterContainer">
+          <img className = "CoverImage" src={Bourne} alt='Bourne Identity Poster'></img>
+            <div className ="Title"><h1>Bourne Identity</h1></div>
+      </div>
      </div>
-    
-     <div className="PosterContainer">
-     <img className = "CoverImage" src={Bourne} alt='Bourne Identity Poster'></img>
-     <div className ="Title"><h1>Bourne Identity</h1></div>
-     </div>
-     </div>
-     <div className="RowContainer">
-      <div className="PosterContainer">
-      <img className = "CoverImage" src={Matrix} alt='Matrix Poster'></img>
-      <div className ="Title"><h1>Matrix</h1></div>
+        <div className="RowContainer">
+          <div className="PosterContainer">
+            <img className = "CoverImage" src={Matrix} alt='Matrix Poster'></img>
+              <div className ="Title"><h1>Matrix</h1></div>
       </div>
-      <div className="PosterContainer">
-      <img className = "CoverImage" src={Fight} alt='Fight Club Poster'></img>
-      <div className ="Title"><h1>Fight Club</h1></div>
-      </div>
-      <div className="PosterContainer">
-      <img className = "CoverImage" src={Alien} alt='Alien Poster'></img>
-      <div className ="Title"><h1>Alien</h1></div>
-      </div>
-      <div className="PosterContainer">
-      <img className = "CoverImage" src={Spider} alt='Spiderman Poster'></img>
-      <div className ="Title"><h1>Spiderman</h1></div>
-      </div>
+          <div className="PosterContainer">
+            <img className = "CoverImage" src={Fight} alt='Fight Club Poster'></img>
+              <div className ="Title"><h1>Fight Club</h1></div>
+            </div>
+          <div className="PosterContainer">
+            <img className = "CoverImage" src={Alien} alt='Alien Poster'></img>
+              <div className ="Title"><h1>Alien</h1></div>
+            </div>
+          <div className="PosterContainer">
+            <img className = "CoverImage" src={Spider} alt='Spiderman Poster'></img>
+              <div className ="Title"><h1>Spiderman</h1></div>
+                </div>
       </div>
     </div>
   );
