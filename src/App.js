14648f50128447502d95607c56e62a5cb3b4909d
r norman <rochellenorman0@gmail.com>
2020-01-07 16:01:53 +0000

    added new posters

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
 import React from 'react';
-import Gooofellas from './assets/Goodfellas.jpg'
+import Gooofellas from './assets/Goodfellas.gif'
+import Leon from './assets/Leon2.jpg'
+import Donnie from './assets/Donnie.jpg'
+import Bourne from './assets/Bourne1.jpg'
+import Alien from './assets/alien.jpeg'
+import Matrix from './assets/matrix.jpg'
+import Fight from './assets/Fight.jpg'
+import Spider from './assets/Spiderman.jpg'
 import './App.css'
 
 function App() {
   return (
-    <div className="App-Container">
-    <div className="First-Row">
-    <div className="First-Movie">
-    <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+    <div className="Container">
+    <div className="RowContainer">
+    <div className="PosterContainer">
+    <img className = "CoverImage" src={Gooofellas} alt='Goofellas Poster'></img>
+    <div className ="Title"><h1>Goodfellas</h1></div>
     </div>
-     <div className="Second-Movie">
-     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
-     </div>
-     <div className="Third-Movie">
-     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
-     </div>
-     <h1>Welcome to my app</h1>
-     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
-     </div>
-     <div className="Second-Row">
-     <div className="First-Movie">
-    <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+    <div className="PosterContainer">
+    <img className = "CoverImage" src={Donnie} alt='Donnie Darko Poster'></img>
+    <div className ="Title"><h1>Donnie Darko</h1></div>
     </div>
-     <div className="Second-Movie">
-     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+    <div className="PosterContainer">
+     <img className = "CoverImage" src={Leon} alt='Leon Poster'></img>
+     <div className ="Title"><h1>Leon</h1></div>
      </div>
-     <div className="Third-Movie">
-     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+    
+     <div className="PosterContainer">
+     <img className = "CoverImage" src={Bourne} alt='Bourne Identity Poster'></img>
+     <div className ="Title"><h1>Bourne Identity</h1></div>
      </div>
      </div>
-     <div className="Third-Row">
-     <div className="First-Movie">
-     <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
-     </div>
-      <div className="Second-Movie">
-      <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+     <div className="RowContainer">
+      <div className="PosterContainer">
+      <img className = "CoverImage" src={Matrix} alt='Matrix Poster'></img>
+      <div className ="Title"><h1>Matrix</h1></div>
       </div>
-      <div className="Third-Movie">
-      <img className = "Cover-Image" src={Gooofellas} alt='Goofellas Poster'></img>
+      <div className="PosterContainer">
+      <img className = "CoverImage" src={Fight} alt='Fight Club Poster'></img>
+      <div className ="Title"><h1>Fight Club</h1></div>
       </div>
-     </div>
+      <div className="PosterContainer">
+      <img className = "CoverImage" src={Alien} alt='Alien Poster'></img>
+      <div className ="Title"><h1>Alien</h1></div>
+      </div>
+      <div className="PosterContainer">
+      <img className = "CoverImage" src={Spider} alt='Spiderman Poster'></img>
+      <div className ="Title"><h1>Spiderman</h1></div>
+      </div>
+      </div>
     </div>
   );
 }
