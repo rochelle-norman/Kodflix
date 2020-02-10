63e689637dc40d0cfb41d49fc022a442efa375c7
r norman <rochellenorman0@gmail.com>
2020-02-10 18:06:26 +0000

    task 18 added a backend server

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,27 @@
 
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 const express = require('express')
+const movieList = require('./movielist')
+const path = require('path')
+
+const listOfMovies = movieList;
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/movies', (req, res) => {
+    res.send(listOfMovies);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
