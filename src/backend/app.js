ba416a4913d5739fd56fd2df38d917517402d56b
r norman <rochellenorman0@gmail.com>
2020-02-14 15:10:45 +0000

    task 23

--- src/backend/app.js
+++ src/backend/app.js
@@ -11,16 +11,16 @@ const movieList = require('./movielist')
 const path = require('path')
 
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.get('/rest/movies', (req, res) => {
     res.send(movieList);
 });
 
-//app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
-//app.get('*', function (req, res) {
-    //res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-//});
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
