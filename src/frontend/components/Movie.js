969f6c6fdaf82c0eb02fb4a2b639778754b7e328
r norman <rochellenorman0@gmail.com>
2020-02-27 14:52:27 +0000

    update json

--- src/frontend/components/Movie.js
+++ src/frontend/components/Movie.js
@@ -2,30 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Movie.css'
 
-export default function Movie(props) {
-    return (
-        <Link to={props.id} className='item'>
-            <img src={`../assets/${props.img}`} alt={`${props.title} 'Poster'`} />
-            <div className="overlay">
-                <h2>{props.title}</h2>
-            </div>
-        </Link>
-    );
+
+
+export default function Movie({ id, title }) {
+  let url = `/${id}`;
+  return (
+    <div className='movie-cover-container'>
+      <div className='movie-cover'>
+
+        <Link to={url}><img src={require(`../assets/${id}.jpg`)} alt={`${title} movie cover`} /></Link>
+      </div>
+      <div className='movie-wall'>
+        <div className="title-overlay">
+          <h2>{`${id}`}</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-// export default function Movie({ id, title }) {
-//   let url = `/${id}`;
-//   return (
-//     <div className='movie-cover-container'>
-//       <div className='movie-cover'>
 
-//         <Link to={url}><img src={require(`../assets/${id}.jpg`)} alt={`${title} movie cover`} /></Link>
-//       </div>
-//       <div className='movie-wall'>
-//         <div className="title-overlay">
-//           <h2>{`${id} (${title})`}</h2>
-//         </div>
-//       </div>
-//     </div>
-//   );
+// export default function Movie(props) {
+//     return (
+//         <Link to={props.id} className='item'>
+//             <img src={`../assets/${props.img}`} alt={`${props.title} 'Poster'`} />
+//             <div className="overlay">
+//                 <h2>{props.title}</h2>
+//             </div>
+//         </Link>
+//     );
 // }
--- src/frontend/components/getMovies.js
+++ src/frontend/components/getMovies.js
@@ -1,11 +1,11 @@
 import Goodfellas from '../assets/Goodfellas.jpg'
 import Leon from '../assets/Leon2.jpg'
 import Donnie from '../assets/Donnie.jpg'
-import Bourne from '../assets/Bourne1.jpg'
-import Alien from '../assets/alien.jpeg'
-import Matrix from '../assets/matrix.jpg'
+import Bourne from '../assets/Bourne.jpg'
+import Alien from '../assets/Alien.jpg'
+import Matrix from '../assets/Matrix.jpg'
 import Spider from '../assets/Spiderman.jpg'
-import Stranger from '../assets/Stranger1.jpg'
+import Stranger from '../assets/Stranger-Things.jpg'
 
 export default function getMovies() {
     return [
@@ -14,7 +14,7 @@ export default function getMovies() {
         { id: 'Leon', title: 'Leon', img: Leon, synopsis: "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. An unusual relationship forms as she becomes his protégée and learns the assassin's trade." },
         { id: 'Matrix', title: 'Matrix', img: Matrix, synopsis: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers." },
         { id: 'Stranger-things', title: 'Stranger Things', img: Stranger, synopsis: "When a young boy disappears, his mother, a police chief and his friends must confront terrifying supernatural forces in order to get him back." },
-        { id: 'Bourne identity', title: 'Bourne Identity', img: Bourne, synopsis: "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, before racing to elude assassins and attempting to regain his memory."},
+        { id: 'Bourne', title: 'Bourne Identity', img: Bourne, synopsis: "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, before racing to elude assassins and attempting to regain his memory."},
         { id: 'Alien', title: 'Alien', img: Alien, synopsis: "After a space merchant vessel receives an unknown transmission as a distress call, one of the crew is attacked by a mysterious life form and they soon realize that its life cycle has merely begun." },
         { id: 'Spiderman', title: 'Spiderman', img: Spider, synopsis: "When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family." }
 
